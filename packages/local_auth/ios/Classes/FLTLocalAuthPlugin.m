24f8923a2f28b3956ad18cf208ffbea5bf76d090
Niall Kenny <niall.s.kenny@gmail.com>
2021-05-24 14:20:45 +0100

    undo changes

--- packages/local_auth/ios/Classes/FLTLocalAuthPlugin.m
+++ packages/local_auth/ios/Classes/FLTLocalAuthPlugin.m
@@ -64,7 +64,7 @@
        flutterResult:(FlutterResult)result
     additionalButton:(NSString *)secondButton {
   UIAlertController *alert =
-      [UIAlertController alertControllerWithTitle:nil
+      [UIAlertController alertControllerWithTitle:@""
                                           message:message
                                    preferredStyle:UIAlertControllerStyleAlert];
 
@@ -122,7 +122,7 @@
   NSError *authError = nil;
   self.lastCallArgs = nil;
   self.lastResult = nil;
-  context.localizedFallbackTitle = nil;
+  context.localizedFallbackTitle = @"";
 
   if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
                            error:&authError]) {
@@ -146,7 +146,7 @@
   NSError *authError = nil;
   _lastCallArgs = nil;
   _lastResult = nil;
-  context.localizedFallbackTitle = nil;
+  context.localizedFallbackTitle = @"";
 
   if (@available(iOS 9.0, *)) {
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&authError]) {
